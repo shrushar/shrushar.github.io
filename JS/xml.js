e6d90664815c3b65cafe34f18598a58e1a60cd62
Shushar <70627183+shrushar@users.noreply.github.com>
2020-12-14 17:23:31 +0300

    Add files via upload

--- JS/xml.js
+++ JS/xml.js
@@ -1,61 +1,45 @@
-var xmlContent ='';
-var about=document.getElementsByClassName("about");
-fetch('/XML/Text.xml').then((response)=>{
-  response.text().then((xml)=>{
-    var parser =new DOMParser();
-    var info=parser.parseFromString(xmlContent,'application/xml');
-    console.log(info);
-    var paragrafs =info.querySelectorAll('paragraf');
-    console.log(paragrafs);
-  });
-  for(var i=0;i<4;i++) {
-    var indent =document.createElement('p');
-    indent.innerHTML= paragrafs[i].textContent.toString();
-    about.appendChild(indent);
-  }
-});
 
-// var Connect = new XMLHttpRequest();
-//   // Define which file to open and
-//   // send the request.
-//   Connect.open("GET", "XML/Text.xml", false);
-//   Connect.setRequestHeader("Content-Type", "text/xml");
-//   Connect.send(null);
-//   // Place the response in an XML document.
-//   var TheDocument = Connect.responseXML;
-//   // Place the root node in an element.
-//   var info = TheDocument.childNodes[1];
-//   var paragrafs =info.getElementsByTagName("paragraf");
-//   var stories=info.getElementsByTagName("story");
-//   var bio=info.getElemegit ntsByTagName("text");
-//   console.log(bio);
-//   console.log(Connect);
-//   console.log(info);
-//   console.log(TheDocument);
-//   console.log(stories);
-//   xml=loadXMLDoc(`../XML/${"Text"}.xml`);
-//   var indent=document.getElementsByClassName("filler");
-//   var character_bio=document.querySelectorAll('.character_info p');
-//   console.log(character_bio);
-// for(var i=0;i<paragrafs.length;i++){
-// 	
-// }
-// for(var i=0;i<bio.length;i++){
-// 	character_bio[i].innerHTML=bio[i].textContent.toString();
-// }
-// var firststory=document.querySelector('.story_tell');
-// firststory.innerHTML=stories[1].textContent.toString();
-// var namen =info.getElementsByTagName("name");
-// var faculty =info.getElementsByTagName("faculty");
-// var spezialisation = info.getElementsByTagName("spezialisation");
-// var year =info.getElementsByTagName("year");
+var Connect = new XMLHttpRequest();
+  // Define which file to open and
+  // send the request.
+  Connect.open("GET", "XML/Text.xml", false);
+  Connect.setRequestHeader("Content-Type", "text/xml");
+  Connect.send(null);
+  // Place the response in an XML document.
+  var TheDocument = Connect.responseXML;
+  // Place the root node in an element.
+  var info = TheDocument.childNodes[1];
+  var paragrafs =info.getElementsByTagName("paragraf");
+  var stories=info.getElementsByTagName("story");
+  var bio=info.getElemegitntsByTagName("text");
+  console.log(bio);
+  console.log(Connect);
+  console.log(info);
+  console.log(TheDocument);
+  console.log(stories);
+  xml=loadXMLDoc(`../XML/${"Text"}.xml`);
+  var indent=document.getElementsByClassName("filler");
+  var character_bio=document.querySelectorAll('.character_info p');
+  console.log(character_bio);
+for(var i=0;i<paragrafs.length;i++){
+	
+}
+for(var i=0;i<bio.length;i++){
+	character_bio[i].innerHTML=bio[i].textContent.toString();
+}
+var firststory=document.querySelector('.story_tell');
+firststory.innerHTML=stories[1].textContent.toString();
+var namen =info.getElementsByTagName("name");
+var faculty =info.getElementsByTagName("faculty");
+var spezialisation = info.getElementsByTagName("spezialisation");
+var year =info.getElementsByTagName("year");
 
-// var footer =document.querySelectorAll('footer p');
-// console.log(footer);
-// console.log(namen);
-// console.log(faculty);
-// console.log(year);
-// footer[0].innerHTML=namen[0].textContent.toString();
-// footer[1].innerHTML=faculty[0].textContent.toString();
-// footer[2].innerHTML=spezialisation[0].textContent.toString();
-// footer[3].innerHTML=year[0].textContent.toString();
+var footer =document.querySelectorAll('footer p');
+console.log(footer);
+console.log(namen);
+console.log(faculty);
+console.log(year);
+footer[0].innerHTML=namen[0].textContent.toString();
+footer[1].innerHTML=faculty[0].textContent.toString();
+footer[2].innerHTML=spezialisation[0].textContent.toString();
+footer[3].innerHTML=year[0].textContent.toString();
