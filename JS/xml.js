88cf5c1d381702adeb9bd9e598c3c2cecd857174
Shushar <70627183+shrushar@users.noreply.github.com>
2020-12-14 17:30:19 +0300

    Add files via upload

--- JS/xml.js
+++ JS/xml.js
@@ -16,7 +16,7 @@ var Connect = new XMLHttpRequest();
   console.log(info);
   console.log(TheDocument);
   console.log(stories);
-  xml=loadXMLDoc(`/XML/${"Text"}.xml`);
+  //xml=loadXMLDoc(`/XML/${"Text"}.xml`);
   var indent=document.getElementsByClassName("filler");
   var character_bio=document.querySelectorAll('.character_info p');
   console.log(character_bio);
