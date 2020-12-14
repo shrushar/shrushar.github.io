c1702e9828318ab708212086b26849f6beb9f90a
Shushar <70627183+shrushar@users.noreply.github.com>
2020-12-14 17:27:20 +0300

    Add files via upload

--- JS/xml.js
+++ JS/xml.js
@@ -1,8 +1,7 @@
-
 var Connect = new XMLHttpRequest();
   // Define which file to open and
   // send the request.
-  Connect.open("GET", "XML/Text.xml", false);
+  Connect.open("GET", "/XML/Text.xml", false);
   Connect.setRequestHeader("Content-Type", "text/xml");
   Connect.send(null);
   // Place the response in an XML document.
@@ -11,18 +10,18 @@ var Connect = new XMLHttpRequest();
   var info = TheDocument.childNodes[1];
   var paragrafs =info.getElementsByTagName("paragraf");
   var stories=info.getElementsByTagName("story");
-  var bio=info.getElemegitntsByTagName("text");
+  var bio=info.getElementsByTagName("text");
   console.log(bio);
   console.log(Connect);
   console.log(info);
   console.log(TheDocument);
   console.log(stories);
-  xml=loadXMLDoc(`../XML/${"Text"}.xml`);
+  xml=loadXMLDoc(`/XML/${"Text"}.xml`);
   var indent=document.getElementsByClassName("filler");
   var character_bio=document.querySelectorAll('.character_info p');
   console.log(character_bio);
 for(var i=0;i<paragrafs.length;i++){
-	
+	indent[i].innerHTML= paragrafs[i].textContent.toString();
 }
 for(var i=0;i<bio.length;i++){
 	character_bio[i].innerHTML=bio[i].textContent.toString();
