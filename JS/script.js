2ff2cc3e538b6b0d1cddcbac92466bc0275d9c83
Shushar <70627183+shrushar@users.noreply.github.com>
2020-12-16 23:59:30 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -84,7 +84,7 @@ buttons.forEach((buttons, index) => {
 	});
 	buttons.addEventListener("scroll",k=>{
 		anime({
-			targets: k,
+			targets: buttons,
 			scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
 			rotateY: {value: '+=180', delay: 200},
 			easing: 'easeInOutSine',
